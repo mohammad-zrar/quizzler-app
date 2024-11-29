30d5746c3dd350b35fbacf8c86177eae0551839a
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-11-29 19:20:55 +0300

    Add category params

--- data.py
+++ data.py
@@ -1,12 +1,13 @@
 import requests
-
 parameters = {
     "amount": 10,
-    "type": "boolean"
+    "type": "boolean",
+    "category": 18,
 }
 
 
 response = requests.get("https://opentdb.com/api.php", params=parameters)
+
 
 question_data = response.json()["results"]
 
